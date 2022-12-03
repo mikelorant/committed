27dc7aaec5658d743265c94703fea825f5284294
Michael Lorant <michael.lorant@nine.com.au>
2022-12-04 09:09:00 +1100

    :tada: Add CLI command
    Add foundation CLI command. This provides no functionality other than
    setting up the framework to begin implementing core capabilities.

---
+++ cmd/root.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	cc "github.com/ivanpirog/coloredcobra"
+	"github.com/spf13/cobra"
+)
+
+func NewRootCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "committed",
+		Short: "A brief description of your application",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return nil
+		},
+	}
+
+	cc.Init(&cc.Config{
+		RootCmd:         cmd,
+		Headings:        cc.HiGreen + cc.Bold,
+		Commands:        cc.HiYellow + cc.Bold,
+		Example:         cc.Italic,
+		ExecName:        cc.Bold,
+		Flags:           cc.Bold,
+		NoExtraNewlines: true,
+		NoBottomNewline: true,
+	})
+
+	return cmd
+}
+
+func Execute() {
+	if err := NewRootCmd().Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/mikelorant/committed
+
+go 1.19
+
+require (
+	github.com/ivanpirog/coloredcobra v1.0.1
+	github.com/spf13/cobra v1.6.1
+)
+
+require (
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+)
