32482400ff9dbda6590768a0634ae70d8bdef460
Michael Lorant <michael.lorant@nine.com.au>
2023-01-07 20:46:55 +1100

    :white_check_mark: Add UI footer component tests
    Add tests for the UI footer component. These tests are using the same
    methods as the other UI components.

---
+++ internal/ui/footer/footer_test.go
@@ -0,0 +1,112 @@
+package footer_test
+
+import (
+	"testing"
+
+	"github.com/hexops/autogold/v2"
+	"github.com/mikelorant/committed/internal/commit"
+	"github.com/mikelorant/committed/internal/repository"
+	"github.com/mikelorant/committed/internal/ui/footer"
+	"github.com/mikelorant/committed/internal/ui/uitest"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestModel(t *testing.T) {
+	type args struct {
+		author repository.User
+		model  func(m footer.Model) footer.Model
+	}
+
+	type want struct {
+		state func(m footer.Model)
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want want
+	}{
+		{
+			name: "default",
+			args: args{
+				author: repository.User{
+					Name:  "John Doe",
+					Email: "john.doe@example.com",
+				},
+			},
+			want: want{
+				state: func(m footer.Model) {
+					u := repository.User{
+						Name:  "John Doe",
+						Email: "john.doe@example.com",
+					}
+
+					assert.Equal(t, u, m.Author)
+					assert.Equal(t, false, m.Signoff)
+					assert.Equal(t, "", m.Value())
+				},
+			},
+		},
+		{
+			name: "signoff",
+			args: args{
+				author: repository.User{
+					Name:  "John Doe",
+					Email: "john.doe@example.com",
+				},
+				model: func(m footer.Model) footer.Model {
+					m.ToggleSignoff()
+					m, _ = footer.ToModel(m.Update(nil))
+					return m
+				},
+			},
+			want: want{
+				state: func(m footer.Model) {
+					u := repository.User{
+						Name:  "John Doe",
+						Email: "john.doe@example.com",
+					}
+
+					assert.Equal(t, u, m.Author)
+					assert.Equal(t, true, m.Signoff)
+					assert.Equal(t, "Signed-off-by: John Doe <john.doe@example.com>", m.Value())
+				},
+			},
+		},
+		{
+			name: "empty",
+			args: args{
+				author: repository.User{},
+			},
+			want: want{
+				state: func(m footer.Model) {
+					u := repository.User{}
+
+					assert.Equal(t, u, m.Author)
+					assert.Equal(t, false, m.Signoff)
+					assert.Equal(t, "", m.Value())
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var c commit.Config
+			c.Repository.Users = []repository.User{tt.args.author}
+
+			m := footer.New(c)
+
+			if tt.args.model != nil {
+				m = tt.args.model(m)
+			}
+
+			if tt.want.state != nil {
+				tt.want.state(m)
+			}
+
+			v := uitest.StripString(m.View())
+			autogold.ExpectFile(t, autogold.Raw(v), autogold.Name(tt.name))
+		})
+	}
+}
