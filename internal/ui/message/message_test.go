b832a15dde4b6aec9b61cdd23009861457aa994b
Michael Lorant <michael.lorant@nine.com.au>
2023-01-08 09:40:04 +1100

    :white_check_mark: Add UI message component tests
    Add tests for the UI message component. There tests are using the same
    methods as the other UI components.
    
    There was no ability to directly change the model so only golden files
    could be compared.

---
+++ internal/ui/message/message_test.go
@@ -0,0 +1,91 @@
+package message_test
+
+import (
+	"testing"
+
+	"github.com/hexops/autogold/v2"
+	"github.com/mikelorant/committed/internal/emoji"
+	"github.com/mikelorant/committed/internal/ui/message"
+	"github.com/mikelorant/committed/internal/ui/uitest"
+)
+
+func TestModel(t *testing.T) {
+	type args struct {
+		emoji   emoji.Emoji
+		summary string
+		body    string
+		footer  string
+	}
+
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "default",
+		},
+		{
+			name: "summary",
+			args: args{
+				summary: "summary",
+			},
+		},
+		{
+			name: "emoji_summary",
+			args: args{
+				emoji: emoji.Emoji{
+					Shortcode: ":art:",
+				},
+				summary: "summary",
+			},
+		},
+		{
+			name: "summary_body",
+			args: args{
+				summary: "summary",
+				body:    "body",
+			},
+		},
+		{
+			name: "summary_body_multiline",
+			args: args{
+				summary: "summary",
+				body:    "line 1\nline 2\nline 3",
+			},
+		},
+		{
+			name: "summary_footer",
+			args: args{
+				summary: "summary",
+				footer:  "footer",
+			},
+		},
+		{
+			name: "all",
+			args: args{
+				emoji: emoji.Emoji{
+					Shortcode: ":art:",
+				},
+				summary: "summary",
+				body:    "body",
+				footer:  "footer",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := message.Config{
+				Emoji:   tt.args.emoji,
+				Summary: tt.args.summary,
+				Body:    tt.args.body,
+				Footer:  tt.args.footer,
+			}
+
+			m := message.New(c)
+
+			v := uitest.StripString(m.View())
+			autogold.ExpectFile(t, autogold.Raw(v), autogold.Name(tt.name))
+		})
+	}
+}
