390e9d9b382e62c796e35e19685a52b2e1465e8a
Michael Lorant <michael.lorant@nine.com.au>
2023-01-08 11:43:02 +1100

    :white_check_mark: Add UI status component tests
    Add tests for the UI status component. These tests are using the same
    methods as the other UI components.
    
    Limited testing as the defaults are always applied and then the next
    and previous values added. A refactor is necessary to allow for
    shortcuts to be completely customisable.

---
+++ internal/ui/status/status_test.go
@@ -0,0 +1,75 @@
+package status_test
+
+import (
+	"testing"
+
+	"github.com/hexops/autogold/v2"
+	"github.com/mikelorant/committed/internal/ui/status"
+	"github.com/mikelorant/committed/internal/ui/uitest"
+)
+
+func TestModel(t *testing.T) {
+	type args struct {
+		shortcuts []status.Shortcut
+		modifiers []status.Modifier
+		next      string
+		previous  string
+	}
+
+	type want struct{}
+
+	tests := []struct {
+		name string
+		args args
+		want want
+	}{
+		{
+			name: "default",
+		},
+		{
+			name: "next",
+			args: args{
+				next: "next",
+			},
+		},
+		{
+			name: "previous",
+			args: args{
+				previous: "previous",
+			},
+		},
+		{
+			name: "next_previous",
+			args: args{
+				next:     "next",
+				previous: "previous",
+			},
+		},
+		{
+			name: "next_previous",
+			args: args{
+				next:     "next",
+				previous: "previous",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := status.New()
+
+			if tt.args.shortcuts != nil && tt.args.modifiers != nil {
+				m.Shortcuts.Shortcuts = tt.args.shortcuts
+				m.Shortcuts.Modifiers = tt.args.modifiers
+			}
+
+			m.Next = tt.args.next
+			m.Previous = tt.args.previous
+
+			m, _ = status.ToModel(m.Update(nil))
+
+			v := uitest.StripString(m.View())
+			autogold.ExpectFile(t, autogold.Raw(v), autogold.Name(tt.name))
+		})
+	}
+}
